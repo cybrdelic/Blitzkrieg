feaa2baf6441e981d206dbc80f99291d172e6d94
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-03 21:51:27 -0700

    implements ContainerManager

---
+++ rundbfast/container_manager.py
@@ -0,0 +1,21 @@
+import time
+from .manager import CommandRunner
+
+class ContainerManager:
+    def __init__(self, container_name):
+        self.container_name = container_name
+        self.runner = CommandRunner()
+
+    def start_container(self):
+        raise NotImplementedError("Subclasses should implement this method.")
+
+    def container_exists(self):
+        existing_containers = self.runner.run_command(f"docker ps -a -q -f name={self.container_name}")
+        return bool(existing_containers)
+
+    def remove_container(self):
+        if self.container_exists():
+            print(f"Container with name {self.container_name} already exists. Stopping and removing...")
+            self.runner.run_command(f"docker stop {self.container_name}")
+            self.runner.run_command(f"docker rm {self.container_name}")
+            time.sleep(5)  # Give Docker a few seconds to free up the name
