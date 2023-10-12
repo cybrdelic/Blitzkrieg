303348b52cbf9c725cc818636bd2499690f78d82
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-12 13:01:38 -0700

    adds integration and unit tests for config

---
+++ tests/test_config_integration.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+from dotenv import load_dotenv
+from rundbfast.config import load_configurations
+
+class TestConfigIntegration(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """Class-level setup function."""
+        load_dotenv(".env.test")  # Assume you have a .env.test file for testing
+
+    def test_default_port(self):
+        """Test if DEFAULT_PORT is loaded correctly."""
+        config = load_configurations()
+        self.assertEqual(config.get("DEFAULT_PORT"), "5432")
+
+    def test_timeout(self):
+        """Test if TIMEOUT is loaded correctly."""
+        config = load_configurations()
+        self.assertEqual(config.get("TIMEOUT"), "60")
+
+    def test_sleep_interval(self):
+        """Test if SLEEP_INTERVAL is loaded correctly."""
+        config = load_configurations()
+        self.assertEqual(config.get("SLEEP_INTERVAL"), "2")
+
+    @classmethod
+    def tearDownClass(cls):
+        """Class-level teardown function."""
+        # Reset the environment variables or do other cleanup tasks.
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_config_units.py
+++ tests/test_config_units.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from unittest.mock import patch
 from dotenv import load_dotenv
 from rundbfast.config import load_configurations
 
@@ -8,27 +9,32 @@ class TestConfig(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Class-level setup function."""
-        load_dotenv(".env.test")  # Assume you have a .env.test file for testing
+        load_dotenv(".env.test")
 
-    def test_default_port(self):
+    @patch('os.getenv')
+    def test_default_port(self, mock_getenv):
         """Test if DEFAULT_PORT is loaded correctly."""
+        mock_getenv.return_value = "5432"
         config = load_configurations()
         self.assertEqual(config.get("DEFAULT_PORT"), "5432")
 
-    def test_timeout(self):
+    @patch('os.getenv')
+    def test_timeout(self, mock_getenv):
         """Test if TIMEOUT is loaded correctly."""
+        mock_getenv.return_value = "60"
         config = load_configurations()
         self.assertEqual(config.get("TIMEOUT"), "60")
 
-    def test_sleep_interval(self):
+    @patch('os.getenv')
+    def test_sleep_interval(self, mock_getenv):
         """Test if SLEEP_INTERVAL is loaded correctly."""
+        mock_getenv.return_value = "2"
         config = load_configurations()
         self.assertEqual(config.get("SLEEP_INTERVAL"), "2")
 
     @classmethod
     def tearDownClass(cls):
         """Class-level teardown function."""
-        # Reset the environment variables or do other cleanup tasks.
         pass
 
 if __name__ == "__main__":
