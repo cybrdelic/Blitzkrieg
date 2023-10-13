8fd9e6d16cccf7554554643bfc41d7d1f198fdbb
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-12 20:33:33 -0700

    changes entry point

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'rundbfast=rundbfast.cli.main:main',
+            'rundbfast=core.cli.main:main',
         ],
     },
     author="Alex Figueroa",
