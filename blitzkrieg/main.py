f1802bc1823c039e1078a080faeb02228c19fcda
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-29 12:09:06 -0700

    auto applies logging to the backend log file of all functions

--- blitzkrieg/main.py
+++ blitzkrieg/main.py
@@ -1,4 +1,8 @@
+from blitzkrieg.cli.logging_config import apply_logger_to_all_functions, setup_logging
 from blitzkrieg.cli.parsing.parser_manager import run
+
+backend_logger, ui_logger, console = setup_logging()
+apply_logger_to_all_functions(backend_logger)
 
 def main():
     run()
