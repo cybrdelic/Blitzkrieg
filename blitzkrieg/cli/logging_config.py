f6fb63185515edb5dd21460e3361f47d4c14db46
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-29 09:51:27 -0700

    adds console to setup_logging output

--- blitzkrieg/cli/logging_config.py
+++ blitzkrieg/cli/logging_config.py
@@ -1,4 +1,5 @@
 import logging
+from rich.console import Console
 
 def setup_logging():
     logging.basicConfig(filename='blitzkrieg_backend.log', level=logging.INFO)
@@ -8,5 +9,6 @@ def setup_logging():
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     ui_logger.addHandler(console_handler)
+    console = Console()
 
-    return backend_logger, ui_logger
+    return backend_logger, ui_logger, console
