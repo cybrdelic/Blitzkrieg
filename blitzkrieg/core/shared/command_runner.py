10cc025aac5bf7f4716d9cc8a6b30dfa9e9a0155
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-10-29 16:45:09 -0700

    enhances ui/ux

--- blitzkrieg/core/shared/command_runner.py
+++ blitzkrieg/core/shared/command_runner.py
@@ -11,7 +11,7 @@ async def _run_command_async(cmd: str) -> str:
     """Run shell commands asynchronously."""
     try:
         with Progress() as progress:
-            task = progress.add_task(f"[cyan]Executing command :::{cmd}:::...[/cyan]", total=100)
+            task = progress.add_task(f"[cyan]Executing command: {cmd}[/cyan]", total=100)
             process = await asyncio.create_subprocess_shell(
                 cmd,
                 stdout=asyncio.subprocess.PIPE,
@@ -20,11 +20,14 @@ async def _run_command_async(cmd: str) -> str:
             stdout, stderr = await process.communicate()
             progress.update(task, completed=100)
 
-        if process.returncode != 0:
-            handle_error(f"Command failed with error: {stderr.decode()}")
-            return None
+            if process.returncode != 0:
+                handle_error(f"Command failed: {stderr.decode().strip()}")
+                console.print("Suggested Action: Please check the command and try again.")
+                return None
 
-        return stdout.decode().strip()
+            console.print("[green]Command executed successfully![/green]")
+            return stdout.decode().strip()
+
     except Exception as e:
-        handle_error(f"An exception occurred: {e}")
+        handle_error(f"An unexpected error occurred: {e}")
         return None
