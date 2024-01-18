daa9b1f8df2a631884d1a92290a6997dd9cb0540
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-01-17 19:05:21 -0800

    fixes issue Model to include proper uuid types

--- blitzkrieg/project_management/db/models/issue.py
+++ blitzkrieg/project_management/db/models/issue.py
@@ -1,18 +1,21 @@
 # sql alchemy model for project issues called issues
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime, Enum
+from sqlalchemy import UUID, Column, Integer, String, ForeignKey, Boolean, DateTime, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from blitzkrieg.project_management.db.models.Base import Base
 class Issue(Base):
-    __tablename__ = 'issues'
+    __tablename__ = 'issue'
+    __table_args__ = {'schema': 'project_management'}
 
-    id = Column(Integer, primary_key=True)
+    id = Column(UUID, primary_key=True)
+    index=Column(Integer)
+    branch_name = Column(String)
     title = Column(String)
     description = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    project_id = Column(Integer, ForeignKey('projects.id'))
+    project_id = Column(UUID, ForeignKey('project_management.project.id'))
 
-    project = relationship('Project', back_populates='issues')
+    project = relationship('Project')
