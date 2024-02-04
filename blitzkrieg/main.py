810373b41e3a41b06fe5415f28bef1580be92beb
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-02-03 22:04:41 -0800

    adds ```gen``` and ```sync``` commands to blitz. gen is to generate classes like CRUD, and Service classes based on all the models in the models dir. sync is to synchronize the database and documents in the editor.

--- blitzkrieg/main.py
+++ blitzkrieg/main.py
@@ -1,6 +1,9 @@
 from blitzkrieg.cli_interface import handle_create_project_command, handle_delete_project_command, handle_link_pgadmin_postgres_command, handle_pgadmin_postgres_init_all_command, handle_pgadmin_postgres_init_command, handle_remove_postgres_pgadmin_command, handle_remover_postgres_pgadmin_command
 import click
 import subprocess
+import os
+
+from blitzkrieg.db.class_generation.DBClassGenerator import DBClassGenerator
 @click.group()
 def main():
     pass
@@ -30,6 +33,14 @@ def remove(project_name):
 def link(project_name_1, project_name_2, parent_name):
     handle_link_pgadmin_postgres_command(project_name_1, project_name_2, parent_name)
 
+# command to sync db and document systems
+@main.command('sync')
+@click.argument('system_to_update')
+def update(system_to_update):
+    """Run the update.sh script."""
+    if system_to_update == 'issues':
+        os.system("python3 blitzkrieg/project_management/db/make/create_issues.py")
+
 @main.command('setup-test')
 def setup_test():
     """Run the setup_test_env.sh script."""
@@ -42,6 +53,14 @@ def create_project():
 @main.command('delete')
 def delete_project():
     handle_delete_project_command()
+
+@main.command('gen')
+@click.argument('what_to_generate')
+def generate_classes(what_to_generate):
+    """Run the generate_classes.sh script."""
+    if what_to_generate == 'classes':
+        generator = DBClassGenerator(models_dir='blitzkrieg/db/models/', templates_dir='blitzkrieg/db/class_generation/templates/')
+        generator.generate_for_all_models()
 
 if __name__ == "__main__":
     click.echo("Starting the application...")
