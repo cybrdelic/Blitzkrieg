67c66577c46d8942a59d27a3da82290de7efabcc
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-04-21 13:36:36 -0700

    implements MarkdownParser class to simplify extract_file_details

--- blitzkrieg/file_management/MarkdownManager.py
+++ blitzkrieg/file_management/MarkdownManager.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 from blitzkrieg.file_management.DirectoryManager import DirectoryManager
-
+from blitzkrieg.file_management.MarkdownParser import MarkdownParser
 from blitzkrieg.project_management.db.services.issues_service import IssueService
 
 class MarkdownManager:
@@ -11,28 +11,12 @@ class MarkdownManager:
         self.markdown_suffix = '.md'
 
     def extract_file_details(self, file_path):
-        # Extracts the object (in this case, issue) ID, title, and content from the file. The ID is in the first line. if it isn't then it probably means it is a new issue that hasnt been synched with the database yet. The title is whatever occurs after a singular #. The content is everything after the title.
         content = self.file_manager.read_file(file_path)
-        lines = content.split('\n')
-
-        # Check if the file has at least one line
-        if len(lines) >= 1:
-            id_line = lines[0]
-        else:
-            id_line = None
-
-        # Find the line with the title
-        title = None
-        for line in lines[1:]:
-            if line.startswith('# '):
-                title = line[2:]  # Remove the hashtag and space
-                break
-
-        # The content is everything after the title
-        content_start_index = lines.index('# ' + title) + 1 if title else 2
-        content = '\n'.join(lines[content_start_index:])
-
-        return id_line.strip() if id_line else None, title.strip() if title else None, content.strip()
+        parser = MarkdownParser(content)
+        issue_id = parser.extract_id(file_path)
+        title = parser.extract_title()
+        content = parser.extract_content()
+        return issue_id, title, content
 
     def verify_uuid_format(self, uuid_to_test, version=4):
         try:
@@ -49,4 +33,3 @@ class MarkdownManager:
         issues_dir = os.path.join(parent_dir, '.docs', 'issues')
         self.directory_manager.validate_directory_presence(issues_dir, 'Issues directory not found in .docs.')
         return self.file_manager.list_files_with_suffix(issues_dir, '.md'), issues_dir
-    
