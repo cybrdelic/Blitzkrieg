b41c59fe30c00a4f997e538ba173c735d66d9a52
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-07-20 18:00:07 -0700

    removes redundant columns from workspace that are now handled by the EnvironmentVariable model

--- blitzkrieg/db/models/workspace.py
+++ blitzkrieg/db/models/workspace.py
@@ -9,17 +9,5 @@ class Workspace(Base):
 
     id = Column(UUID, primary_key=True)
     name = Column(String)
-    pgadmin_port = Column(String)
-    pgadmin_username = Column(String)
-    pgadmin_password = Column(String)
-    postgres_port = Column(String)
-    postgres_username = Column(String)
-    postgres_host = Column(String)
-    postgres_db = Column(String)
-    postgres_password = Column(String)
     description = Column(String)
-    project_id = Column(UUID, ForeignKey('project_management.project.id'))
     path = Column(String)
-    sqlalchemy_uri = Column(String)
-
-    project = relationship('Project')
