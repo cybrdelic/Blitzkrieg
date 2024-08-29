e0aafc41a6fc7ddc6eb314d33926100223bfb71f
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-08-28 21:49:08 -0700

    adds new columns to Project

--- blitzkrieg/db/models/project.py
+++ blitzkrieg/db/models/project.py
@@ -8,7 +8,6 @@ class Project(Base):
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4)
     name = Column(String)
-    description = Column(String)
     github_repo = Column(String)  # GitHub repository URL
     directory_path = Column(String)  # Path to the project directory
     is_deployed = Column(Boolean, default=False)  # Whether the project is deployed
@@ -17,6 +16,9 @@ class Project(Base):
     parent_id = Column(UUID, ForeignKey('project_management.project.id'))  # ID of the parent project
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
+    short_description = Column(String, nullable=True)  # Short description of the project
+    description = Column(String, nullable=True)  # Description of the project
+    # project_type should be an Enum of python_cli, pyo3_rust_extension, etc.
+    project_type = Column(Enum('python_cli', 'pyo3_rust_extension', name='project_type_enum'))
     # Relationship to self to allow nested projects
     children = relationship('Project', backref='parent', remote_side=[id])
