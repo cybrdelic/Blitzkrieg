4f8ff67ede1316e84ae7a822a80f108cc1e34cab
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-09-08 11:53:31 -0700

    adds ExtrapolationTypesEnum

---
+++ blitzkrieg/enums/extrapolation_types.py
@@ -0,0 +1,9 @@
+from enum import Enum as PyEnum
+
+class ExtrapolationTypesEnum(PyEnum):
+    README_CONTEXTUALIZATION_SYNOPSIS = 'readme_contextualization_synopsis'
+    COMMAND_FLOW_SYNOPSIS = 'command_flow_synopsis'
+    MAIN_IDEAS_SYNOPSIS = 'main_ideas_synopsis'
+    PROJECT_STRUCTURE_SYNOPSIS = 'project_structure_synopsis'
+    PROJECT_HIGH_LEVEL_OVERVIEW = 'project_high_level_overview'
+    PROJECT_DEPLOYMENT_SYNOPSIS = 'project_deployment_synopsis'
