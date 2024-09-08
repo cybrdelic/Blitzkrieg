cec03d2a70730db9d730a9af50ef61a9c06ee4c5
cybrvybe <alexfigueroa.cybr@gmail.com>
2024-09-08 12:13:07 -0700

    simplifies console instance

--- blitzkrieg/ui_management/console_instance.py
+++ blitzkrieg/ui_management/console_instance.py
@@ -1,14 +1,3 @@
 from blitzkrieg.ui_management.ConsoleInterface import ConsoleInterface
-from blitzkrieg.ui_management.tui import TextualApp
 
 console = ConsoleInterface()
-def create_tui_app(commands=None):
-    app = TextualApp(commands)
-    return app
-
-def run_tui(commands=None):
-    app = create_tui_app(commands)
-    app.run()
-
-# Global instances for backward compatibility
-app = create_tui_app()
